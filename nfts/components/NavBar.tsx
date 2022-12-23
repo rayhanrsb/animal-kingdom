a0f158c30eb524cbc9d28c31cca9e11c233c9a30
Rayhan Beebeejaun <44209174+rayhanrsb@users.noreply.github.com>
2022-12-23 14:33:20 +0400

    deploy to vercel

--- nfts/components/NavBar.tsx
+++ nfts/components/NavBar.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { HStack, Spacer } from "@chakra-ui/react";
 import { FC } from 'react';
 import styles from "../styles/Home.module.css";
@@ -15,22 +14,4 @@ export default function NavBar () {
             <WalletMultiButtonDynamic className={styles["wallet-adapter-button-trigger"]} />
         </HStack>
     )
-=======
-import { HStack, Spacer } from "@chakra-ui/react";
-import { FC } from 'react';
-import styles from "../styles/Home.module.css";
-import dynamic from "next/dynamic";
-
-const WalletMultiButtonDynamic = dynamic(
-    async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton, { ssr: false }
-);
-
-export default function NavBar () {
-    return (
-        <HStack width="full" padding="4">
-            <Spacer />
-            <WalletMultiButtonDynamic className={styles["wallet-adapter-button-trigger"]} />
-        </HStack>
-    )
->>>>>>> 205c421d3d8d866de5a33a3100d4134d345c9951
 };
