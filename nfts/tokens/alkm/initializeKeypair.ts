1bfcc7e9eca063a5d08fcb8f07b74428a68eb2b6
Rayhan Beebeejaun <44209174+rayhanrsb@users.noreply.github.com>
2022-12-23 14:36:59 +0400

    deploy to vercel

--- nfts/tokens/alkm/initializeKeypair.ts
+++ nfts/tokens/alkm/initializeKeypair.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as web3 from "@solana/web3.js";
 import * as fs from "fs";
 import dotenv from "dotenv";
@@ -49,55 +48,3 @@ async function airdropSolIfNeeded(
     console.log("New balance is", newBalance / web3.LAMPORTS_PER_SOL)
   }
 }
-=======
-import * as web3 from "@solana/web3.js";
-import * as fs from "fs";
-import dotenv from "dotenv";
-dotenv.config()
-
-export async function initializeKeypair(
-  connection: web3.Connection
-): Promise<web3.Keypair> {
-  if (!process.env.PRIVATE_KEY) {
-    console.log("Creating .env file")
-    const signer = web3.Keypair.generate()
-    fs.writeFileSync(".env", `PRIVATE_KEY=[${signer.secretKey.toString()}]`)
-    await airdropSolIfNeeded(signer, connection)
-
-    return signer
-  }
-
-  const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[]
-  const secretKey = Uint8Array.from(secret)
-  const keypairFromSecretKey = web3.Keypair.fromSecretKey(secretKey)
-  await airdropSolIfNeeded(keypairFromSecretKey, connection)
-  return keypairFromSecretKey
-}
-
-async function airdropSolIfNeeded(
-  signer: web3.Keypair,
-  connection: web3.Connection
-) {
-  const balance = await connection.getBalance(signer.publicKey)
-  console.log("Current balance is", balance / web3.LAMPORTS_PER_SOL)
-
-  if (balance < web3.LAMPORTS_PER_SOL) {
-    console.log("Airdropping 1 SOL...")
-    const airdropSignature = await connection.requestAirdrop(
-      signer.publicKey,
-      web3.LAMPORTS_PER_SOL
-    )
-
-    const latestBlockHash = await connection.getLatestBlockhash()
-
-    await connection.confirmTransaction({
-      blockhash: latestBlockHash.blockhash,
-      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-      signature: airdropSignature,
-    })
-
-    const newBalance = await connection.getBalance(signer.publicKey)
-    console.log("New balance is", newBalance / web3.LAMPORTS_PER_SOL)
-  }
-}
->>>>>>> 205c421d3d8d866de5a33a3100d4134d345c9951
